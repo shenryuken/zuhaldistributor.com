be9d814fca01b44188f8f7ed2cf6c6cbed0456c7
shenryuken <shenryuken@yahoo.com>
2020-02-12 22:12:54 +0800

    Add InfyOm Code Generator

--- config/app.php
+++ config/app.php
@@ -165,7 +165,8 @@ return [
         /*
          * Package Service Providers...
          */
-
+        \InfyOm\GeneratorBuilder\GeneratorBuilderServiceProvider::class, 
+        \InfyOm\RoutesExplorer\RoutesExplorerServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -225,6 +226,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form'      => Collective\Html\FormFacade::class,
+        'Html'      => Collective\Html\HtmlFacade::class,
+        'Flash'     => Laracasts\Flash\Flash::class,
 
     ],
 
